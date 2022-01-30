057a4a3d78d0666d63d9058e1fe8acedcc6050d7
Ramon Mota <ramon_mota@live.com>
2022-01-30 15:40:28 -0300

    visual structure

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from './img/iconWeather/Clouds3.svg';
+import './app.scss'
 
 import api from './services/api'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='ContainerBox'>
+      <a className='selectCity'>Calgary</a>
+      <div className='displyTemperature'>
+        <div className='temperatureVariable'>
+          <h3>21</h3>
+          <label className='elicpeCelsius'></label>
+          <p>Min</p>
+        </div >
+        <div className='temperature'>
+          <h3>28</h3>
+          <label className='elicpeCelsius'></label>
+        </div>
+        <div className='temperatureVariable'>
+          <h3>35</h3>
+          <label className='elicpeCelsius'></label>
+          <p>Max</p>
+        </div>
+      </div>
+      <img className='iconWeather' src={logo}/>
+      <p className='descriptionWeather'>nuvens quebradas</p>
     </div>
   );
 }
--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,9 @@
 import axios from "axios";
 
+let keyAPI = "f1528275ba90d8b0f63b16b770aecc82"
+
 const api = axios.create({
-    baseURL: "https://api.openweathermap.org/data/2.5/weather?q=Calgary&appid=f1528275ba90d8b0f63b16b770aecc82"
+    baseURL: `https://api.openweathermap.org/data/2.5/weather?q=Calgary&appid=${keyAPI}`
 })
 
 export default api;
